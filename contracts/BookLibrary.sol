422f81bf347932b5a6bf817bcb403a2a69656bfa
i.iliev <i.iliev@allterco.com>
2023-05-29 15:32:41 +0300

    Refactor BookLibrary contract

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -7,88 +7,54 @@ contract BookLibrary is Ownable {
     struct Book {
         string name;
         uint16 copies;
+        address[] bookBorrowedAddresses;
     }
 
-    struct Borrow {
-        uint bookId;
-        address user;
-        uint32 borrowedAt;
-        uint32 returnedAt;
-    }
+    bytes32[] public bookKey;
 
-    uint availableBooksCount = 0;
+    event LogNewBook(string name, uint16 copies);
+    event LogNewBorrow(address user, string name);
+    event LogNewReturn(address user, string name);
 
-    Book[] private books;
-    Borrow[] private borrows;
+    mapping(bytes32 => Book) public books;
+    mapping(address => mapping(bytes32 => bool)) public borrowedBooks;
 
-    event NewBook(string name, uint16 copies);
-    event NewBorrow(address user, string name, uint time);
-    event NewReturn(address user, string name, uint time);
-
-    mapping (uint => uint16) private copiesAvailable; // Available copies for each book
-    mapping (uint => address[]) private bookRenters; // Keeps history of all renters of a book
-
-    modifier isBooked (uint bookId) {
-        bool found = false;
-        for (uint i = 0; i < borrows.length; i++) {
-            if (borrows[i].bookId == bookId && borrows[i].user == msg.sender) {
-                found = true;
-                break;
-            }
-        }
-        require(!found, "Book is already borrowed by this user!");
+    modifier isBooked (bytes32 bookId) {
+        require(books[bookId].copies > 0, "No copies left from this book");
+        require(!borrowedBooks[msg.sender][bookId], "This book is already borrowed by this user");
         _;
     }
 
     function addBook(string memory _name, uint16 _copies) external onlyOwner {
-        require(_copies > 0);
-        books.push(Book(_name, _copies));
-        uint id = books.length - 1;
-        copiesAvailable[id] = _copies;
-        availableBooksCount++;
-        emit NewBook(_name, _copies);
+        require(_copies > 0, "Copies must be at least 1");
+        require(bytes(_name).length > 0, "Name is not valid");
+        bytes32 _bookKey = keccak256(abi.encodePacked(_name));
+        require(bytes(books[_bookKey].name).length == 0, "Book is already added");
+
+        address[] memory borrowed;
+        Book memory newBook = Book(_name, _copies, borrowed);
+        books[_bookKey] = newBook;
+        bookKey.push(_bookKey);
+        emit LogNewBook(_name, _copies);
     }
 
-    function availableBooks() external view returns(string[] memory) {
-        string[] memory result = new string[](availableBooksCount);
-        uint counter = 0;
-        for (uint i = 0; i < books.length; i++) {
-            if (copiesAvailable[i] > 0) {
-                result[counter] = books[i].name;
-            }
-            counter++;
-        }
-
-        return result;
+    function borrowBook(bytes32 _bookId) external isBooked(_bookId) {
+        Book storage book = books[_bookId];
+        borrowedBooks[msg.sender][_bookId] = true;
+        book.bookBorrowedAddresses.push(msg.sender);
+        book.copies--;
+        emit LogNewBorrow(msg.sender, books[_bookId].name);
     }
 
-    function borrow(uint _bookId) external isBooked(_bookId) {
-        require(copiesAvailable[_bookId] > 0);
-        require(availableBooksCount > 0);
-        uint32 time = uint32(block.timestamp);
-
-        bookRenters[_bookId].push(msg.sender);
-        borrows.push(Borrow(_bookId, msg.sender, time, 0));
-        copiesAvailable[_bookId]--;
-        if (copiesAvailable[_bookId] == 0) {
-            availableBooksCount--;
-        }
-
-        emit NewBorrow(msg.sender, books[_bookId].name, time);
+    function returnBook(bytes32 _bookId) external {
+        Book storage book = books[_bookId];
+        borrowedBooks[msg.sender][_bookId] = false;
+        book.copies++;
+        emit LogNewReturn(msg.sender, books[_bookId].name);
     }
 
-    function returnBook(uint _bookId) external {
-        uint32 time = uint32(block.timestamp);
-
-        for (uint i = 0; i < borrows.length; i++) {
-            if (borrows[i].bookId == _bookId) {
-                require(borrows[i].borrowedAt < time);
-                borrows[i].returnedAt = time;
-                availableBooksCount++;
-                copiesAvailable[_bookId]++;
-            }
-        }
-
-        emit NewReturn(msg.sender, books[_bookId].name, time);
+    function getAllAddressesBorrowedBook(bytes32 _bookId) external view returns (address[] memory _book) {
+        Book memory book = books[_bookId];
+        return book.bookBorrowedAddresses;
     }
 }
